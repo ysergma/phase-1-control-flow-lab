704f057b1020e8abbd76e21be6d60242b999a329
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-07-23 14:30:43 +0100

    completed assignement

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400) {
+    return 'This one is on me!';
+  }
+  else if (ride >= 400 && ride <= 2000  ) {
+    return 'That will be twenty bucks.';
+  } 
+  else if (ride > 2000 && ride <= 2500 ) {
+    return 'I will gladly take your thirty bucks.';
+  } 
+  else {
+    return 'No can do.';
+  } 
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC') {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+    return "Thank you so much.";
+    break;
+   case 'not as generous':
+    return  "Thank you.";
+    break;
+   default:
+    return  "Bye.";
+  }
 }
